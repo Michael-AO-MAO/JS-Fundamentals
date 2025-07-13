fa6af78fe1e9046b28ff47690e8808a0625b8da4
Michael-AO-MAO <mikeaddoowusu@gmail.com>
2025-07-12 16:22:30 -0800

    Add files via upload

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,9 @@
 const x = Number(process.argv[2]);
 
-if (Number.isInteger(x) && x >= 0) {
-  for (let i = 0; i < x; i++) console.log("C is fun");
-} else {
+if (process.argv[2] === undefined || isNaN(x) || !Number.isInteger(x)) {
   console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
